a5481fa908bcb9ba90ac2bced00fe105ff1a88b8
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2023-05-30 23:53:35 +0200

    Add postgres DB on docker container and prisma orm

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+  signin() {
+    // NestJS automatically convert the data type based on the return
+    // returning a string would send a text/HTML as content type
+    // returning an object would send a application/json as content type
+    return { msg: 'Signed in' };
+  }
+
+  signup() {
+    return { msg: 'Signed up' };
+  }
+}
