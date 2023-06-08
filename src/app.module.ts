756cb4dc54d56fe29f0e2a45c95773ea531a9b17
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2023-06-08 17:18:29 +0200

    [ADD] config module to use env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,18 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
+// The ConfigModule from nestjs/config allows us to load the .env file into the app (using dotenv under the hood)
+// Thx to importing it, we can use the config service in the other modules
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [
+    // We could use the @Global decorator to make the ConfigModule global in the app
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 // PrismaClient is a class that allows to connect to the DB
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     // super will call the constructor of the class being extended (PrismaClient). Since the
     // constructor of PrismaClient needs to have datasources, db and url props we pass it to the super
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:abc123.@localhost:5434/nest?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
